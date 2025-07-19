53850fae690258dc414e8f2adbb8b0d65a5f4bb1
Ben <emmanuelbeneboh@gmail.com>
2025-07-18 21:56:29 -0400

    menu switching with a click event handler

--- takeout/src/components/Navbar/Navbar.jsx
+++ takeout/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+
   return (
     <div className="navbar">
       <img src={assets.logo} alt="TakeOut Logo" className="logo" />
       <ul className="nav-links">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile App</li>
-        <li>Contact Us</li>
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setMenu("Mobile-App")}
+          className={menu === "Mobile-App" ? "active" : ""}
+        >
+          Mobile App
+        </li>
+        <li
+          onClick={() => setMenu("Contact Us")}
+          className={menu === "Contact Us" ? "active" : ""}
+        >
+          Contact Us
+        </li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="Search" className="search-icon" />
