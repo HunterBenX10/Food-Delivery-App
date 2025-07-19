a7527d7f35c5b3d94bd0719ea8bdb4f3e746b65f
Ben <emmanuelbeneboh@gmail.com>
2025-07-18 20:19:45 -0400

    Navbar completed

--- takeout/src/components/Navbar/Navbar.jsx
+++ takeout/src/components/Navbar/Navbar.jsx
@@ -6,6 +6,20 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <img src={assets.logo} alt="TakeOut Logo" className="logo" />
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Mobile App</li>
+        <li>Contact Us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="Search" className="search-icon" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="Cart" className="cart-icon" />
+          <div className="dot"></div>
+        </div>
+        <button className="login-button">Login</button>
+      </div>
     </div>
   );
 };
