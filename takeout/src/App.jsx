1d32c4a7fff311c751847a71a9b2bbd2d05350a9
Ben <emmanuelbeneboh@gmail.com>
2025-07-19 23:44:36 -0400

    added React Router Dom and routed pages for Cart, Home, and PlaceOrder

--- takeout/src/App.jsx
+++ takeout/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 };
